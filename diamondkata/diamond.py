c3638289a29f5e512f092ec575d667fdbe61a5dc
Choi Jung Hyun <cjhnim@gmail.com>
2020-02-14 10:49:10 +0900

    diamond kata refactoring

--- diamondkata/diamond.py
+++ diamondkata/diamond.py
@@ -1,2 +1,53 @@
+
 class Diamond:
-    pass
+    START_LETTER = 'A'
+    ENTER = '\n'
+    SPACE = ' '
+    BLANK = ''
+
+    def __init__(self, upTo):
+        self.upTo = upTo
+    
+    def show(self):
+        top = self.BLANK
+        bottom = self.BLANK
+        
+        for c in range(ord(self.START_LETTER), ord(self.upTo)+1):
+            line = self.buildLine(chr(c))
+            top += line
+            bottom = self.bottomLine(chr(c), line, bottom)
+
+        return top + bottom
+
+    def bottomLine(self, c, line, bottom):
+        return line + bottom if(c != self.upTo) else bottom
+
+    def buildLine(self, c):
+        return self.indent(c) + \
+                self.letter(c) + \
+                self.innerSpace(c) + \
+                self.secondLetter(c) + \
+                self.ENTER
+    
+    def letter(self, c):
+        return c
+
+    def secondLetter(self, c):
+        return c if c != self.START_LETTER else ''
+
+    def indent(self, c):
+        count = ord(self.upTo) - ord(c)
+        return self.makeSpace(count)
+
+    def innerSpace(self, c):
+        index = ord(c) - ord(self.START_LETTER)
+        count = index*2-1
+        return self.makeSpace(count)
+
+    def makeSpace(self, count):
+        space = ''
+        for i in range(0, count):
+            space += self.SPACE
+        return space
+
+        
--- diamondkata/diamond_test.py
+++ diamondkata/diamond_test.py
@@ -1,54 +1,5 @@
 import unittest
-# import diamond
-
-class Diamond:
-    START_LETTER = 'A'
-    ENTER = '\n'
-    SPACE = ' '
-    BLANK = ''
-
-    def __init__(self, upTo):
-        self.upTo = upTo
-    
-    def show(self):
-        top = self.BLANK
-        bottom = self.BLANK
-        
-        for c in range(ord(self.START_LETTER), ord(self.upTo)+1):
-            line = self.buildLine(chr(c))
-            top += line
-            if(chr(c) != self.upTo):
-                bottom = line + bottom
-
-        return top + bottom
-
-    def buildLine(self, c):
-        line = self.indent(c)
-        line += c
-        if c != self.START_LETTER:
-            line += self.innerSpace(c)
-            line += c
-        line += self.ENTER
-        return line
-
-    def indent(self, c):
-        count = ord(self.upTo) - ord(c)
-        return self.makeSpace(count)
-
-    def innerSpace(self, c):
-        index = ord(c) - ord(self.START_LETTER)
-        count = index*2-1
-        return self.makeSpace(count)
-
-    def makeSpace(self, count):
-        space = ''
-        for i in range(0, count):
-            space += self.SPACE
-        return space
-
-
-
-            
+from diamond import Diamond
 
 class DiamondTests(unittest.TestCase):
 
@@ -76,6 +27,19 @@ class DiamondTests(unittest.TestCase):
                    "  A\n"
              
         self.assertEqual(expected, d.show())
+    
+    def test_DisABBCCDDBBA(self):
+        d = Diamond('D')
+        expected = "   A\n" + \
+                   "  B B\n" + \
+                   " C   C\n" + \
+                   "D     D\n" + \
+                   " C   C\n" + \
+                   "  B B\n" + \
+                   "   A\n"
+             
+        self.assertEqual(expected, d.show())
+        
 
 if __name__ == '__main__':
     unittest.main()
